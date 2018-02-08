0770004b4c7c3d6f465fa965bd2b9d4e491e2aa7
krohak <rohaksinghal14@gmail.com>
2018-02-08 16:10:14 +0800

    update

--- Permutations/perm.py
+++ Permutations/perm.py
@@ -1,12 +1,12 @@
-def permute(A,j):
-    if j == len(A):
+def permute(A,i):
+    if i == len(A):
         print(A)
         return
 
-    for i in range(j,len(A)):
-        A[j], A[i] = A[i], A[j]
-        permute(A, j+1)
-        A[j], A[i] = A[i], A[j]
+    for j in range(i,len(A)):
+        A[i], A[j] = A[j], A[i]
+        permute(A, i+1)
+        A[i], A[j] = A[j], A[i]
 
 A = [1,2,3]
 permute(A,0)
