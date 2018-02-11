6bd094184720b6a95094fe19cf78ee1bebb7deb4
krohak <rohaksinghal14@gmail.com>
2018-02-11 17:14:50 +0800

    use map instead of unordered_map

--- Sort Two Strings/sortstrings.cpp
+++ Sort Two Strings/sortstrings.cpp
@@ -1,45 +1,66 @@
 #include <iostream>
 #include <string>
 #include <unordered_map>
+#include <map>
 
 using namespace std;
 
 
 string sort_strings(string first, string second){
-    unordered_map<char,int> hash_table;
-    unordered_map<char,int>::iterator itr_map;
+        map<char,int> hash_table;
+        map<char,int>::iterator itr_map;
 
-    for (int i=0; i<first.length(); i++){
-      if (!hash_table.count(first[i])){
-        hash_table[first[i]] = 0;
-      }
-      else{
-        hash_table[first[i]] += 1;
-      }
-    }
-
-    for (int i=0; i<second.length(); i++){
-      if (!hash_table.count(second[i])){
-        hash_table[second[i]] = 0;
-      }
-      else{
-        hash_table[second[i]] += 1;
-      }
-    }
-
-    string third = "";
-
-
-    for(char j = 'a'; j<= 'z'; j++ ){
-      if(hash_table.count(j)){
-        for(int i = 0; i<=hash_table[j]; i++){
-          third+=j;
-          //cout << j;
+        for (int i=0; i<first.length(); i++) {
+                if (!hash_table.count(first[i])) {
+                        hash_table[first[i]] = 0;
+                }
+                else{
+                        hash_table[first[i]] += 1;
+                }
         }
-      }
-    }
 
-    return third;
+        for (int i=0; i<second.length(); i++) {
+                if (!hash_table.count(second[i])) {
+                        hash_table[second[i]] = 0;
+                }
+                else{
+                        hash_table[second[i]] += 1;
+                }
+        }
+
+        string third = "";
+
+
+        /*
+           // For unordered_map
+
+           for(char j = 'a'; j<= 'z'; j++ ){
+            if(hash_table.count(j)){
+              for(int i = 0; i<=hash_table[j]; i++){
+                third+=j;
+                //cout << j;
+              }
+            }
+           }*/
+
+
+        // For ordered map
+
+        // Vanilla
+        /*  for (itr_map = hash_table.begin(); itr_map != hash_table.end(); itr_map++){
+            for (int i = 0; i<=(*itr_map).second; i++){
+              third+=(*itr_map).first;
+            }
+           }*/
+
+        // Range
+        for (auto elem: hash_table) {
+                for (int i=0; i<= elem.second; i++) {
+                        third+= elem.first;
+                }
+        }
+
+        return third;
 
 }
 
@@ -47,8 +68,8 @@ string sort_strings(string first, string second){
 
 int main(){
 
-  string result = sort_strings("wdcefvrgbtg","ujyeeeeeeeeeeehbtgvrfd");
-  cout << result << endl;
+        string result = sort_strings("wdcefvrgbtg","ujllllllhbtgvrfd");
+        cout << result << endl;
 
-  return 0;
+        return 0;
 }
