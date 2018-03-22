af75224c98445c4e5ad74211bb5d59bc43f5c4dc
krohak <rohaksinghal14@gmail.com>
2018-03-22 16:42:39 +0800

    modify

--- HackerRank/BFS/bfs-dfs.py
+++ HackerRank/BFS/bfs-dfs.py
@@ -4,39 +4,37 @@ def Graph(object):
         self.edges=defaultdict(set)
         self.n=n
         self.distances=[]
+        self.nodes=set()
 
     def connect(x,y):
         self.edges[x].add(y)
         self.edges[y].add(x)
+        self.nodes.add(x)
+        self.nodes.add(y)
 
     def bfs(self, initial):
-
         frontier=[]
         explored=set()
 
         frontier.append(node)
 
         while(frontier):
-
         node = frontier.pop(0)
         explored.add(node)
-
             for neighbour in self.edges[node]:
                 if neighbour not in explored and neighbour not in frontier:
                     frontier.append(neighbour)
 
 
-    def call_dfs(self,initial):
+    def call_dfs(self):
         explored=set()
-        dfs(initial)
+        for node in self.nodes:
+            dfs(node)
 
     def dfs(self, initital):
-
         if not self.edges[initial]:
             return
-
         explored.add(initital)
-
         for neighbour in self.edges[node]:
             if neighbour not in explored:
                 dfs(neighbour)
