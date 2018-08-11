cfa44f52993114064e449c7819458defbcc344b2
krohak <krohak@hku.hk>
2018-08-11 15:20:10 +0800

    Solution in python

--- LeetCode/Hard/Basic Calculator II/sol.py
+++ LeetCode/Hard/Basic Calculator II/sol.py
@@ -1,5 +1,4 @@
-symbols = ['/','*','-','+']
-symbols_set = set(symbols)
+symbols_set = set(['/','*','+','-',' '])
 
 def isSymbol(char):
     global symbols_set
@@ -39,9 +38,37 @@ def expressionToList(expression):
 
 
 def evaluateList(expression_list):
-    return 0
+    md = -1
+    sign = 1
+    result = 0
+    prev = 0
 
-
+    i = 0
+    while i<len(expression_list):
+        if not isSymbol(expression_list[i]):
+            if md == 0:
+                prev *= expression_list[i]
+                md = -1
+            elif md ==  1: 
+                prev //= expression_list[i]
+                md = -1
+            else:
+                prev = expression_list[i]
+        
+        else:
+            if expression_list[i] == '/':
+                md = 1
+            elif expression_list[i] == '*':
+                md = 0
+            elif expression_list[i] == '+':
+                result += sign*prev
+                sign = 1
+            elif expression_list[i] == '-':
+                result += sign*prev
+                sign = -1   
+        i+=1
+    result += sign*prev
+    return result
 
 
 class Solution(object):
@@ -50,3 +77,8 @@ class Solution(object):
         expression_list = expressionToList(expression)
         result = evaluateList(expression_list)
         return result    
+
+
+expression = "3+2*2"
+result = Solution().calculate(expression)
+print(result)
