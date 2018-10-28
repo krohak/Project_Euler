678746ee08d8eb8e6ff691d84d6493a2cbd92769
krohak <rohaksinghal14@gmail.com>
2018-10-29 02:11:08 +0800

    working solution

--- LeetCode/Medium/3Sum/sol.py
+++ LeetCode/Medium/3Sum/sol.py
@@ -1,48 +1,44 @@
+class Solution(object):
+    def threeSum(self, nums):
 
-# a+b+c = 0 ?
-# from hashlib import sha224
-# sha224(str(l).encode('utf-8')).hexdigest()
+        ans = []
 
-def threeSUM(arr):
+        nums.sort()
+        
+        n = len(nums)
 
-    pos_map = {}
+        for i in range(n):
 
-    for elem in arr:
-        if elem not in pos_map:
-            pos_map[elem] = 1
-        else:
-            pos_map[elem]+=1
-
-    arr = set(arr)
-    arr = list(arr)
-    print(arr)
-    
-    sol = []
-
-    n = len(arr)
-    i = 0
-
-    while i<n:
-        j = i+1
-        while j<n:
-
-            pos_map[arr[i]]-=1
-            pos_map[arr[j]]-=1
-            remaining = -(arr[i] + arr[j])
-
-            if remaining in pos_map:
-                if pos_map[remaining] > 0:
-                    sol.append([arr[i],arr[j],remaining])
+            if (i > 0 and nums[i] == nums[i - 1]):
+                 continue
             
-            pos_map[arr[i]]+=1
-            pos_map[arr[j]]+=1
+            left = i + 1
+            right = n - 1
 
-            j+=1
-        i+=1
+            while (left < right) :
 
-    return sol
+                total = nums[i] + nums[left] + nums[right]
+                
+                if (total == 0):
+                    
+                    ans.append([nums[i], nums[left], nums[right]])
+
+                    while(left < right and nums[left] == nums[left + 1]):
+                         left+=1
+                    while(left < right and nums[right] == nums[right - 1]):
+                         right-=1
+
+                    left+=1
+                    right-=1
+
+                elif (total < 0):
+                    left+=1
+                else:
+                    right-=1
+                
+        return ans
 
 
-arr = [-1,0,1,2,-1,-4]
-sol = threeSUM(arr)
+nums = [-1,0,1,2,-1,-4]
+sol = Solution().threeSum(nums)
 print(sol)
