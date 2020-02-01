0810c58d6c876c036f7d4de28eb843244dde7259
krohak <rohaksinghal14@gmail.com>
2020-02-01 13:20:58 +0800

    Revise

--- LeetCode/Medium/BT Indorder Iterative/revise/sol.py
+++ LeetCode/Medium/BT Indorder Iterative/revise/sol.py
@@ -1,32 +1,37 @@
-# Definition for a binary tree node.
-class TreeNode:
+class TreeNode(object):
     def __init__(self, x):
         self.val = x
         self.left = None
         self.right = None
 
+'''
+Think about it in recursive terms and then
+formulate an iterative solution using stack
+to keep track of recursive calls.
+
+At a point, you will need to keep a pointer
+to skip lines of code in the recursive call. Use None
+to skip re-traversal of left sub tree and pop the call stack.
+'''
 class Solution:
-    def inorderTraversal(self, root: TreeNode) -> List[int]:
-
-
+    def inorderTraversal(self, root: TreeNode):
+        
         stack = []
-        ans = []
-        curr = root
+        answer = []
+        cursor = root
 
-        while(stack or curr):
+        while(stack or cursor):
 
-            if(not curr):
-                curr = stack.pop()
-                ans.append(curr.val)
-                curr = curr.right
+            while(cursor):
+                stack.append(cursor.val)
+                cursor = cursor.left
 
-            elif(curr.left):
-                stack.append(curr)
-                curr = curr.left
-                
+            cursor = stack.pop()
+            answer.append(cursor)
             
-            elif(not curr.left):
-                ans.append(curr.val)
-                curr = curr.right
+            cursor = cursor.right
 
-        return(ans)
+
+
+
+
