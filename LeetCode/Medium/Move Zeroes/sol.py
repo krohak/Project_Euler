29f1fcf0e057014bc2136454a91f2cf2548b0e2e
krohak <rohaksinghal14@gmail.com>
2020-02-08 16:54:53 +0800

    uff

--- LeetCode/Medium/Move Zeroes/sol.py
+++ LeetCode/Medium/Move Zeroes/sol.py
@@ -3,4 +3,30 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        pass
+        
+        length_nums = len(nums)
+        
+        position = 0
+        non_zero = 0
+        
+        # find first zero 
+        while(position < length_nums and not nums[position] == 0 ):
+            position+=1
+        
+        # nothing to do
+        if position == length_nums:
+              return 
+        
+        
+        non_zero = position + 1       
+        
+        while(non_zero < length_nums):
+            
+            if not nums[non_zero] == 0:
+                nums[position] = nums[non_zero]
+                nums[non_zero] = 0
+                position+=1
+            
+            non_zero +=1
+        
+        return 
