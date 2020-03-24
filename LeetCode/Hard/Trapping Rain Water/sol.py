18df8baa9c9a64779c9460f8807beabc139b124f
krohak <rohaksinghal14@gmail.com>
2020-03-24 21:04:35 +0800

    we are the champions

--- LeetCode/Hard/Trapping Rain Water/sol.py
+++ LeetCode/Hard/Trapping Rain Water/sol.py
@@ -1,33 +1,24 @@
 class Solution:
     def trap(self, heights):
         
-        if not heights:
+        n = len(heights)
+        if not n:
             return 0
         
-        len_heights = len(heights)
+        left_heights = [heights[0]]
+        for i in range(1,n):
+            left_heights.append(max(left_heights[-1], heights[i]))
         
-        left_highest = [0 for _ in heights]
-        left_highest[0] = heights[0]
         
-        for i in range(1, len_heights):
-            if heights[i] > left_highest[i-1]:
-                left_highest[i] = heights[i]
-            else:
-                left_highest[i] = left_highest[i-1]
+        right_heights = [0 for _ in range(n)]
+        right_heights[n-1] = heights[n-1]
+        for i in range(n-2, -1, -1):
+            right_heights[i] = max(right_heights[i+1], heights[i])
         
-        right_highest = [ 0  for _ in heights]
-        right_highest[len_heights-1] = heights[len_heights-1]
         
-        for i in range(len_heights-2, -1, -1):
-            if heights[i] > right_highest[i+1]:
-                right_highest[i] = heights[i]
-            else:
-                right_highest[i] = right_highest[i+1]
+        ans = 0
         
-        water_trapped = 0
-        for i in range(len_heights):
-            to_add = min(left_highest[i], right_highest[i]) - heights[i]
-            water_trapped+=to_add
-            
+        for i in range(1, n-1):
+            ans += min(left_heights[i], right_heights[i])-heights[i]
         
-        return water_trapped
+        return ans
