400d6b6119d6c152cd344a67997171c4c980ac4d
krohak <rohaksinghal14@gmail.com>
2020-03-25 10:15:07 +0800

    sol

--- LeetCode/Medium/BT Zigzag/sol.py
+++ LeetCode/Medium/BT Zigzag/sol.py
@@ -1,11 +1,10 @@
 class Solution:
     def zigzagLevelOrder(self, root):
-
+        
         if not root:
             return []
-
+            
         ans = []
-        direction = 1
 
         stack_right = []
         stack_left = []
@@ -14,25 +13,24 @@ class Solution:
         while(stack_left or stack_right):
             
             ans_nest = []
-
-            if direction:
                 
-                while(stack_right):
-                    curr = stack_right.pop()
-                    ans_nest.append(curr.val)
-                    stack_left.append(curr.left) if curr.left else None
-                    stack_left.append(curr.right) if curr.right else None
+            while(stack_right):
+                curr = stack_right.pop()
+                ans_nest.append(curr.val)
+                stack_left.append(curr.left) if curr.left else None
+                stack_left.append(curr.right) if curr.right else None
                 
-
-            else:
+            ans.append(ans_nest) if ans_nest else None
                 
-                while(stack_left):
-                    curr = stack_left.pop()
-                    ans_nest.append(curr.val)
-                    stack_right.append(curr.right) if curr.right else None
-                    stack_right.append(curr.left) if curr.left else None
+            
+            ans_nest = []
+            while(stack_left):
+                curr = stack_left.pop()
+                ans_nest.append(curr.val)
+                stack_right.append(curr.right) if curr.right else None
+                stack_right.append(curr.left) if curr.left else None
 
-            ans.append(ans_nest)
-            direction = 1-direction             
+            ans.append(ans_nest) if ans_nest else None
+          
 
         return ans
