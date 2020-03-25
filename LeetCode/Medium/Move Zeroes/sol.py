73659541e97423e6c54c06b2a4da761fbfdd7e62
krohak <rohaksinghal14@gmail.com>
2020-03-25 12:35:45 +0800

    better

--- LeetCode/Medium/Move Zeroes/sol.py
+++ LeetCode/Medium/Move Zeroes/sol.py
@@ -1,32 +1,16 @@
 class Solution:
-    def moveZeroes(self, nums):
+    def moveZeroes(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
         
         length_nums = len(nums)
         
-        position = 0
-        non_zero = 0
+        found_zero_at = 0
         
-        # find first zero 
-        while(position < length_nums and not nums[position] == 0 ):
-            position+=1
+        for position in range(length_nums):
+            if not nums[position] == 0:
+                nums[position], nums[found_zero_at] = nums[found_zero_at], nums[position]
+                found_zero_at+=1
+
         
-        # nothing to do
-        if position == length_nums:
-              return 
-        
-        
-        non_zero = position + 1       
-        
-        while(non_zero < length_nums):
-            
-            if not nums[non_zero] == 0:
-                nums[position] = nums[non_zero]
-                nums[non_zero] = 0
-                position+=1
-            
-            non_zero +=1
-        
-        return 
