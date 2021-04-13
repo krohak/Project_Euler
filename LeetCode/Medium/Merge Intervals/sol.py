cc6a77751d1d76b61d8a154b1b106a66ace1a026
krohak <rohaksinghal14@gmail.com>
2021-04-13 11:05:47 +0800

    better solution

--- LeetCode/Medium/Merge Intervals/sol.py
+++ LeetCode/Medium/Merge Intervals/sol.py
@@ -1,40 +1,21 @@
 class Solution:
     def merge(self, intervals):
         
-        def custom_comparator(x):
-            return x[0]
+        if len(intervals) < 2:
+            return intervals
         
-        intervals = sorted(intervals, key=custom_comparator)
-
-        first_index = 0
-        second_index = 1
+        sortedIntervals  = sorted(intervals) #nlogn
+        merged = [sortedIntervals[0]]
         
-        length_intervals = len(intervals)
+        for i in range(1, len(sortedIntervals)):
+            _, endx =  merged[-1]
+            starty, endy = sortedIntervals[i]
+            
+            if starty <= endx:                
+                if endy > endx:
+                    merged[-1][-1] = endy
+                
+            else:
+                merged.append([starty, endy])
         
-        while (second_index<length_intervals):
-            first_interval = intervals[first_index]
-            second_interval = intervals[second_index]
-            
-            end_first_interval = first_interval[1]
-            
-            start_second_interval = second_interval[0]
-            end_second_interval = second_interval[1]
-            
-            if start_second_interval<= end_first_interval:
-                
-                if end_second_interval<= end_first_interval:
-                    pass
-                
-                else:
-                    first_interval[1] = end_second_interval
-                
-                intervals[second_index] = None
-                
-                
-            if (intervals[second_index]):
-                first_index = second_index
-                
-            second_index+=1
-            
-        intervals = [ interval for interval in intervals if interval ]
-        return intervals
+        return merged
