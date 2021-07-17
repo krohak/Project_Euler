960b0d9df0afc3f11c867f12ea3a926b94c9e675
krohak <rohaksinghal14@gmail.com>
2021-07-17 16:37:32 +0800

    heapsort

--- HackerRank/Sort/Heap/sol.py
+++ HackerRank/Sort/Heap/sol.py
@@ -1,17 +1,18 @@
 import heapq
 
-def heap_sort(arr):
-
+def heapSort(arr):
     heapq.heapify(arr)
-
     ans = []
     while arr:
         elem = heapq.heappop(arr)
         ans.append(elem)
-
     return ans
 
-
-arr = [3,6,10,2,7,4,5,9,1,8,3,6,10,2,7,4,5,9,1,8]
-ans = heap_sort(arr)
-print(ans)
+import numpy as np
+for k in range(1):
+    n=100
+    randomList = np.random.rand(100)*100
+    print(randomList)
+    randomList = heapSort(list(randomList))
+    print(randomList)
+    print(all(randomList[i] <= randomList[i+1] for i in range(len(randomList)-1)))
