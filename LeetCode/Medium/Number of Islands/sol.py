55a78aa0c7973c43813eecd4b9bb58a999bf3e32
krohak <rohaksinghal14@gmail.com>
2021-07-19 14:18:35 +0800

    better

--- LeetCode/Medium/Number of Islands/sol.py
+++ LeetCode/Medium/Number of Islands/sol.py
@@ -1,50 +1,31 @@
 class Solution:
-
-    def __init__(self):
-        self.grid = []
-        self.m = 0
-        self.n = 0
-
-    def numIslands(self, grid):
-        self.grid = grid
-        self.m = len(grid)
-        if not self.m:
-            return 0
-        self.n = len(grid[0])
-        if not self.n:
-            return 0
-
-        num_islands = 0
-
-        explored = set()
-
-        for i in range(self.m):
-            for j in range(self.n):
-                if (i,j) not in explored and grid[i][j] == "1":
-                    num_islands += 1
-                    self.dfs(i, j, explored)
-
-        return num_islands
-
     
-    def dfs(self, i, j, explored):
-
-        explored.add((i,j))
-
-        for (ni, nj) in self.neighbours(i,j):
-            if (ni, nj) not in explored:
-                self.dfs(ni, nj, explored)
-
-    def neighbours(self, i, j):
-        ngbs = []
-
-        if i-1>=0 and self.grid[i-1][j] == "1":
-            ngbs.append((i-1, j))
-        if j-1>=0 and self.grid[i][j-1] == "1":
-            ngbs.append((i, j-1))
-        if i+1<self.m and self.grid[i+1][j] == "1":
-            ngbs.append((i+1, j))
-        if j+1<self.n and self.grid[i][j+1] == "1":
-            ngbs.append((i, j+1))
-
-        return ngbs
+    def numIslands(self, grid):
+        
+        m = len(grid)
+        if not m: return 0
+        
+        n = len(grid[0])
+        if not n: return 0
+        
+        connectedComponents = 0
+        self.visited = set()
+        self.grid = grid
+        
+        for i in range(m):
+            for j in range(n):
+                if self.grid[i][j]=="1" and (i,j) not in self.visited:
+                    connectedComponents+=1
+                    self.dfs(i, j)
+        return connectedComponents
+    
+    def dfs(self, i, j):
+        self.visited.add((i, j))
+        for (ni, nj) in self.getNeighbors(i,j):
+            if (ni,nj) not in self.visited:
+                self.dfs(ni, nj)
+    
+    def getNeighbors(self, i, j):
+        checkGrid = lambda x,y: 0<=x and x<len(self.grid) and 0<=y and y<len(self.grid[0]) and self.grid[x][y]=="1"
+        return [ ( i+x, j+y) for x, y in [[0, +1], [0, -1], [-1, 0], [+1, 0]] if checkGrid(i+x,j+y)  ]
+        
