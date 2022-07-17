000cd5d3f14c2aa607c9ff97e374a26ffba6466a
krohak <rohaksinghal14@gmail.com>
2022-07-17 15:18:40 +0800

    DS

--- LeetCode/Medium/LRU Cache/sol.py
+++ LeetCode/Medium/LRU Cache/sol.py
@@ -1,36 +1,17 @@
 from collections import OrderedDict
-class LRUCache(OrderedDict):
 
-    def __init__(self, capacity):
-        """
-        :type capacity: int
-        """
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.lru = OrderedDict()
         self.capacity = capacity
-
-    def get(self, key):
-        """
-        :type key: int
-        :rtype: int
-        """
-        if key not in self:
-            return - 1
         
-        self.move_to_end(key)
-        return self[key]
-
-    def put(self, key, value):
-        """
-        :type key: int
-        :type value: int
-        :rtype: void
-        """
-        if key in self:
-            self.move_to_end(key)
-        self[key] = value
-        if len(self) > self.capacity:
-            self.popitem(last = False)
-
-# Your LRUCache object will be instantiated and called as such:
-# obj = LRUCache(capacity)
-# param_1 = obj.get(key)
-# obj.put(key,value)
+    def get(self, key: int) -> int:
+        if key in self.lru: 
+            self.lru.move_to_end(key, last=False)
+        return self.lru.get(key, -1)
+        
+    def put(self, key: int, value: int) -> None:
+        self.lru[key] = value
+        self.lru.move_to_end(key, last=False)
+        if len(self.lru) > self.capacity:
+            self.lru.popitem()
